956127902afb60ba6ed9b30e0d1e462b34ac89aa
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-15 23:43:23 -0300

    fix : aplicando responsividade

--- meu-portfolio/src/sections/Home.js
+++ meu-portfolio/src/sections/Home.js
@@ -1,7 +1,6 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 
 
 import React from 'react';
@@ -9,45 +8,41 @@ import Wrapper from '../components/Wrapper';
 
 const Home = () => {
   return (
-    <Wrapper sectionId="inicio" minHeight="100vh">
+    <Wrapper sectionId='inicio' minHeight='100vh'>
       <Box
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           flexDirection: 'column',
           textAlign: 'center',
-          width: '28rem',
-          height: '20rem',
+          width: { xs: '100%', sm: '100%', md: '448px' },
+          height: {xs: '350px' , sm: '320px', md: '320px'},
           padding: '15px 0',
-          margin: '5.5rem 5.5rem 0 5.5rem ',
+          margin: { xs: '12px 0', sm: '50px 5px', md: '80px' },
           opacity: '80%',
           backgroundColor: '#80d8ff',
-          borderRadius:"20px"
+          borderRadius: '20px',
         }}
       >
         <Box
           sx={{
-            padding: '0 10px',
+            padding: { xs: '5px', sm: '5px', md: '0 10px' },
             opacity: '100%',
           }}
         >
-           <Typography
-            variant="h3"
-            component="div"
-            color="secondary"
-            sx={{ 
-              flexGrow: 1, 
-              display: { xs: 'none', sm: 'block' },
-              color: '#01579b',
-              fontFamily: 'Gamja Flower, cursive',
-              fontSize: "2.5rem",
-              fontWeight: '900',    
+          <Typography
+            variant='h3'
+            component='div'
+            color="#01579b"
+            sx={{
+              padding: '10px',
+              fontFamily: 'Fredoka One, cursive',
+              fontSize: { xs: '1.5rem', sm: '1.75rem', md: '2rem' },
             }}
           >
-         Bem Vindo ao meu Portfólio!
-
+            Bem Vindo ao meu Portfólio!
           </Typography>
-       
+
         </Box>
 
         <Box
@@ -55,118 +50,106 @@ const Home = () => {
             opacity: '100%',
           }}
         >
-           <Typography
-            variant="h2"
-            component="div"
-            color="secondary"
-            sx={{ 
-              flexGrow: 1, 
-              display: { xs: 'none', sm: 'block' },
-              color: '#1a237e',
+          <Typography
+            variant='h2'
+            component='div'
+            color='#1a237e'
+            sx={{
               fontFamily: 'Gamja Flower, cursive',
-              fontSize: "4rem",
-              fontWeight: '900',    
+              fontSize: '4rem',
+              fontWeight: '900',
             }}
           >
-          Jeferson Gomes
+            Jeferson Gomes
           </Typography>
 
           <Typography
-            variant="h2"
-            component="div"
-            color="secondary"
-            sx={{ 
-              flexGrow: 1, 
-              display: { xs: 'none', sm: 'block' },
-              color: '#01579b',
-              fontFamily: 'Gamja Flower, cursive',
-              fontSize: "1.5rem",
-              fontWeight: '900',    
+            variant='h2'
+            component='div'
+            color='#01579b'
+            sx={{
+              fontFamily: 'Fredoka One, cursive',
+              fontSize: '1.5rem',
             }}
           >
-          Desenvolvedor Full Stack
+            Desenvolvedor Full Stack
           </Typography>
         </Box>
 
         <Box
           sx={{
-            display: "flex",
+            display: 'flex',
             justifyContent: "space-evenly",
             opacity: '100%',
           }}
         >
-        
-         <Button
+
+          <Button
             variant="contained"
-            size="large"
-            component="a"
-            href="https://www.linkedin.com/in/jefersongjr/"
-            target="_blank"
+            size='large'
+            component='a'
+            href='https://www.linkedin.com/in/jefersongjr/'
+            target='_blank'
             sx={{
               transition: '400ms',
               width: '60px',
               backgroundColor: '#80d8ff',
             }}
           >
-            <img src='https://img.icons8.com/doodle/512/linkedin-circled.png' 
-            width={50}
-            alt='icone-likedin'
+            <img src='https://img.icons8.com/doodle/512/linkedin-circled.png'
+              width={50}
+              alt='icone-likedin'
             />
-            </Button>
-            
-            <Button
-            variant="contained"
-            size="large"
-            component="a"
-            href="https://drive.google.com/file/d/1phzXvxI1RgfsV4-ELFojIR69_gjKcs_K/view?usp=share_link"
-            target="_blank"
+          </Button>
+
+          <Button
+            variant='contained'
+            size='large'
+            component='a'
+            href='https://drive.google.com/file/d/1phzXvxI1RgfsV4-ELFojIR69_gjKcs_K/view?usp=share_link'
+            target='_blank'
             sx={{
               transition: '400ms',
               width: '150px',
               backgroundColor: '#80d8ff',
             }}
           >
-            <img src='https://img.icons8.com/clouds/512/resume.png' 
-            width={50}
-            alt='icone-likedin'/>
-          <Typography
-            variant="p"
-            component="div"
-            color="secondary"
-            sx={{ 
-              flexGrow: 1, 
-              display: { xs: 'none', sm: 'block' },
-              color: '#01579b',
-              fontFamily: 'Gamja Flower, cursive',
-              fontSize: "1rem",
-              fontWeight: '900',    
-            }}
-          >
-          Currículo
-          </Typography>            </Button>
+            <img src='https://img.icons8.com/clouds/512/resume.png'
+              width={50}
+              alt='icone-likedin' />
+            <Typography
+              variant='p'
+              component='div'
+              color='#01579b'
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', sm: 'block' },
+                fontFamily: 'Fredoka One, cursive',
+                fontSize: '1rem',
+              }}
+            >
+              Currículo
+            </Typography>            </Button>
 
-                    
-         <Button
-            variant="contained"
-            size="large"
-            component="a"
-            href="https://github.com/jefersongjr"
-            target="_blank"
+
+          <Button
+            variant='contained'
+            size='large'
+            component='a'
+            href='https://github.com/jefersongjr'
+            target='_blank'
             sx={{
               transition: '200ms',
               width: '60px',
               backgroundColor: '#80d8ff',
             }}
           >
-            <img src='https://img.icons8.com/doodle/512/github.png' 
-            width={50}
-            alt='icone-likedin'/>
-            </Button>
-          </Box>
-
-       
+            <img src='https://img.icons8.com/doodle/512/github.png'
+              width={50}
+              alt='icone-likedin' />
+          </Button>
+        </Box>
       </Box>
-
     </Wrapper>
   );
 }
