5fdcdf3d9335b62263aa7f37065b221224757282
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-16 00:26:58 -0300

    feat: criaando a página sobre

---
+++ meu-portfolio/src/sections/Sobre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from '../components/Wrapper';
+
+const Sobre = () => {
+  return (
+    <Wrapper sectionId='Sobre' minHeight='100vh'>
+ 
+    </Wrapper>
+  );
+}
+
+export default Sobre;
