9c8e07752f73c1bac0896e48c5501d890543d0fa
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-21 01:48:10 -0300

    feat :sparkles: terminando o header

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [language, setLanguage] = useState('EN');
+
   return (
     <div>
-      <Header />
+      <Header setLanguage={ setLanguage } language={ language } />
       <main>
         OLA
+        <p>{language}</p>
       </main>
     </div>
   );
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,21 +1,70 @@
 import logo from '../assets/logotipo.png';
 import logoGithub from '../assets/icons-github.png';
 import logoLinkedin from '../assets/icons-linkedin.png';
+import portuguese from '../assets/icons-brazil.png';
+import english from '../assets/icons-usa.png';
 
-function Header() {
+const sections = [
+  {
+    name: {
+      EN: 'HOME',
+      BR: 'INÍCIO',
+    },
+    href: '#home',
+  },
+  {
+    name: {
+      EN: 'ABOUT',
+      BR: 'SOBRE',
+    },
+    href: '#about',
+  },
+  {
+    name: {
+      EN: 'PROJECTS',
+      BR: 'PROJETOS',
+    },
+    href: '#projects',
+  },
+  {
+    name: {
+      EN: 'STACKS',
+      BR: 'TECNOLOGIAS',
+    },
+    href: '#projects',
+  },
+  {
+    name: {
+      EN: 'CONTACT',
+      BR: 'CONTATO',
+    },
+    href: '#contact',
+  },
+];
+
+function Header({ setLanguage, language }) {
   return (
     <header className="header-container">
       <img src={ logo } alt="meu logo" width="100px" />
       <nav className="nav-container">
         <ul className="navigation">
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>PROJECTS</li>
-          <li>STACKS</li>
-          <li>CONTATOS</li>
+          {sections.map(({ name, href }) => (
+            <li key={ name[language] }>
+              <a href={ href }>
+                {name[language]}
+              </a>
+            </li>))}
         </ul>
       </nav>
       <div className="icons-container">
+        <button onClick={ () => setLanguage('BR') } className="language-button">
+          <img src={ portuguese } alt="" width="25px" />
+          BR
+        </button>
+        <button onClick={ () => setLanguage('EN') } className="language-button">
+          <img src={ english } alt="" width="25px" />
+          EN
+        </button>
         <img src={ logoGithub } alt="logo-github" width="25px" />
         <img src={ logoLinkedin } alt="logo-linkedin" width="30px" />
       </div>
