bc55d609d534704f605fcf9465ba5a4b452d82d5
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-21 00:36:30 -0300

    feat :sparkles: adicionando imagens no header

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,8 +1,12 @@
+import logo from '../assets/logotipo.png';
+import logoGithub from '../assets/icons-github.png';
+import logoLinkedin from '../assets/icons-linkedin.png';
+
 function Header() {
   return (
     <header className="header-container">
-      <img src="/logo.png" alt="meu logo" />
-      <nav className="class-container">
+      <img src={ logo } alt="meu logo" width="100px" />
+      <nav className="nav-container">
         <ul className="navigation">
           <li>HOME</li>
           <li>ABOUT</li>
@@ -11,7 +15,10 @@ function Header() {
           <li>CONTATOS</li>
         </ul>
       </nav>
-      <div />
+      <div className="icons-container">
+        <img src={ logoGithub } alt="logo-github" width="25px" />
+        <img src={ logoLinkedin } alt="logo-linkedin" width="30px" />
+      </div>
     </header>
   );
 }
