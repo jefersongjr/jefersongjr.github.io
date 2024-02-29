723f1a24ad569fca67a7f909548822883c51f592
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-28 21:26:27 -0300

    feat :sparkles: adicionando card do front online store

--- my-portfolio/src/utils/dataImg.jsx
+++ my-portfolio/src/utils/dataImg.jsx
@@ -1,11 +1,13 @@
 import safeInternet from '../assets/imgs/projectsPreview/safeInternet.png';
 import tfc from '../assets/imgs/projectsPreview/tfc.png';
 import trivia from '../assets/imgs/projectsPreview/trivia.png';
+import store from '../assets/imgs/projectsPreview/store.png';
 
 const previewData = {
   1: safeInternet,
   2: tfc,
   3: trivia,
+  4: store,
 };
 
 export default previewData;
