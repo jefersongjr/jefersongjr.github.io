b86853dec136f3286e4370ec0f0dda520d72dad6
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-29 21:55:59 -0300

    feat :sparkles: criando data de skills  do back end

--- my-portfolio/src/utils/data.jsx
+++ my-portfolio/src/utils/data.jsx
@@ -10,12 +10,21 @@ const {
   redux,
   sass,
   tailwind,
-  boot } = stacksData;
+  boot,
+  node,
+  express,
+  sequelize,
+  docker,
+  python,
+  mysql,
+  postgre,
+  mongo,
+  mongose } = stacksData;
 
 const skillsByCategory = {
   front: [html, css, js, react, router, typescript, redux, sass, tailwind, boot],
-  front: [html, css, js, react, router, typescript, redux, sass, tailwind, boot],
-
+  back: [js, node, typescript, express, sequelize, docker,
+    python, mysql, postgre, mongo, mongose],
 };
 
 export default skillsByCategory;
