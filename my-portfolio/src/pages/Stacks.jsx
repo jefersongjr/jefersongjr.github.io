7e03e2161a14ad5626fa2a116b2f9f20b08bb0a2
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-29 22:14:08 -0300

    feat :sparkles: renderizando skills do back-end

--- my-portfolio/src/pages/Stacks.jsx
+++ my-portfolio/src/pages/Stacks.jsx
@@ -8,21 +8,31 @@ const data = {
   },
 };
 
+const { front, back } = skillsByCategory;
+
 function Stacks({ language }) {
   return (
     <section id="stacks" className="stacks-container">
       <h3>{data.title[language]}</h3>
       <div className="line-about" />
       <div className="card-stacks-container">
-        <div className="card-stacks">
-          {skillsByCategory.front.map(({ name, icon }) => (
+        <div className="card-stacks-front">
+          {front.map(({ name, icon }) => (
             <div className={ name } key={ name }>
               <span>{name}</span>
               { icon }
             </div>
           ))}
         </div>
-        <div className="card-stacks">Node</div>
+
+        <div className="card-stacks">
+          {back?.map((stack) => (
+            <div className={ stack.name } key={ stack.name }>
+              <span>{stack.name}</span>
+              {stack.icon}
+            </div>
+          ))}
+        </div>
         <div className="card-stacks">Jest</div>
       </div>
     </section>
