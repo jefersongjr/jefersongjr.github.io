81c0bdce2441567922d7c45fe6bcc885d82ff2b4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-29 22:29:42 -0300

    feat :sparkles: renderizando skills de test

--- my-portfolio/src/pages/Stacks.jsx
+++ my-portfolio/src/pages/Stacks.jsx
@@ -8,7 +8,7 @@ const data = {
   },
 };
 
-const { front, back } = skillsByCategory;
+const { front, back, test } = skillsByCategory;
 
 function Stacks({ language }) {
   return (
@@ -17,6 +17,7 @@ function Stacks({ language }) {
       <div className="line-about" />
       <div className="card-stacks-container">
         <div className="card-stacks-front">
+          <h4>Front End</h4>
           {front.map(({ name, icon }) => (
             <div className={ name } key={ name }>
               <span>{name}</span>
@@ -26,6 +27,7 @@ function Stacks({ language }) {
         </div>
 
         <div className="card-stacks">
+          <h4>Back End</h4>
           {back?.map((stack) => (
             <div className={ stack.name } key={ stack.name }>
               <span>{stack.name}</span>
@@ -33,7 +35,15 @@ function Stacks({ language }) {
             </div>
           ))}
         </div>
-        <div className="card-stacks">Jest</div>
+        <div className="card-stacks">
+          <h4>Tests</h4>
+          {test?.map((stack) => (
+            <div className={ stack.name } key={ stack.name }>
+              <span>{stack.name}</span>
+              {stack.icon}
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
