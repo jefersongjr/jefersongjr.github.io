1b6b04483a19c3c1e8ed3088e8e02e158c77bf99
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-29 21:49:36 -0300

    feat :sparkles: renderizando skills do front

--- my-portfolio/src/pages/Stacks.jsx
+++ my-portfolio/src/pages/Stacks.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import skillsByCategory from '../utils/data';
 
 const data = {
   title: {
@@ -12,6 +13,18 @@ function Stacks({ language }) {
     <section id="stacks" className="stacks-container">
       <h3>{data.title[language]}</h3>
       <div className="line-about" />
+      <div className="card-stacks-container">
+        <div className="card-stacks">
+          {skillsByCategory.front.map(({ name, icon }) => (
+            <div className={ name } key={ name }>
+              <span>{name}</span>
+              { icon }
+            </div>
+          ))}
+        </div>
+        <div className="card-stacks">Node</div>
+        <div className="card-stacks">Jest</div>
+      </div>
     </section>
   );
 }
