b90867662959cec738b3ff39db4ed0368a7b3eb4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-01 22:04:52 -0300

    feat :sparkles: criando section contacts

---
+++ my-portfolio/src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+const data = {
+  title: {
+    EN: `Let's Start a 
+    Conversation`,
+    BR: 'Vamos Iniciar uma Conversa',
+  },
+};
+
+function Contacts({ language }) {
+  return (
+    <section id="contacts" className="contacts-container">
+      <h3>{data.title[language]}</h3>
+    </section>
+  );
+}
+
+Contacts.propTypes = {
+  language: PropTypes.string.isRequired,
+};
+
+export default Contacts;
