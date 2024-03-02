081081c0f83ea55a49c7e1883c516f5ff74f2bc5
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-01 22:33:00 -0300

    feat :sparkles: adicionando divisor

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -12,6 +12,7 @@ function Contacts({ language }) {
   return (
     <section id="contacts" className="contacts-container">
       <h3>{data.title[language]}</h3>
+      <div className="line-contacts" />
     </section>
   );
 }
