b803a6268e1f1338d427936b7f5595d3f56b9265
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-01 23:04:23 -0300

    feat :sparkles: criando forms de envio de email

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -6,6 +6,26 @@ const data = {
     Conversation`,
     BR: 'Vamos Iniciar uma Conversa',
   },
+  name: {
+    EN: 'My name is',
+    BR: 'Meu nome é',
+  },
+  name_placeholder: {
+    EN: 'Name',
+    BR: 'Nome',
+  },
+  email: {
+    EN: 'and my email is',
+    BR: 'e meu email é',
+  },
+  interest: {
+    EN: 'i am interested in',
+    BR: 'Estou interessado em',
+  },
+  interest_placeholder: {
+    EN: 'Send me your message',
+    BR: 'Envie-me sua mensagem',
+  },
 };
 
 function Contacts({ language }) {
@@ -13,6 +33,23 @@ function Contacts({ language }) {
     <section id="contacts" className="contacts-container">
       <h3>{data.title[language]}</h3>
       <div className="line-contacts" />
+      <form action="https://formspree.io/jeferson_gjr@hotmail.com" method="POST" className="forms-container">
+        <div className="forms-top-container">
+          <label className="label-name">
+            {data.name[language]}
+            <input type="text" placeholder={ data.name_placeholder[language] } />
+          </label>
+          <label className="label-email">
+            {data.name[language]}
+            <input type="text" placeholder={ data.name_placeholder[language] } />
+          </label>
+        </div>
+        <label className="label-message">
+          {data.interest[language]}
+          <input type="text" placeholder={ data.interest_placeholder[language] } />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
     </section>
   );
 }
